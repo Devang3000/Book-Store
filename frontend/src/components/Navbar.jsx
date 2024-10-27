9a1b18cda5eb523d80b807768328d747ed8fed55
Devang3000 <devangpathak3@gmail.com>
2024-10-27 16:36:30 +0530

    27-10-2024

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+const Navbar = () => {
+  return (
+    <header className = "max-w-screen-2xl mx-auto px-4 py-6">
+      <nav className = "flex justify-between items-center">
+        {/*left side*/}
+        <div>
+        <Link to="/">Logo</Link>
+
+        </div>
+        
+        {/*right side*/}
+        <div>
+          nav items
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
